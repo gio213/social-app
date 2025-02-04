787beb2ea79fc3028b6cca894b6829b06ae53b72
Giorgi Patsia <gio.patsia@gmail.com>
2025-02-04 11:36:00 +0100

    added user db save and fetching this user

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavBar";
 import MobileNavbar from "./MobileNavBar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-const NavBar = () => {
+const NavBar = async () => {
+  const user = await currentUser();
+
+  if (user) await syncUser();
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
